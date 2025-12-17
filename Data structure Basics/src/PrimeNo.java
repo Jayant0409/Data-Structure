dbc8df8396fa990e54c06403483ec7bcaf995a13
Jayant <004jayant@gmail.com>
2025-12-17 23:07:40 +0530

    Basics

---
+++ Data structure Basics/src/PrimeNo.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class PrimeNo {
+
+    public static int printPrime(int num){
+
+        int n =2;
+
+        while(n<=num){
+            boolean isPrime = true;
+//            if(n==2){
+//                System.out.println(n);
+//            }
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if(n%i==0)
+                    isPrime = false;
+            }
+            if(isPrime == true){
+                System.out.println("Prime no. "+ n);
+            }
+            n++;
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("enter the number");
+
+        int num = Sc.nextInt();
+        printPrime(num);
+
+
+
+    }
+}
